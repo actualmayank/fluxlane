cdc20655288ca545033647bbae4a3b4a02cad1e3
Mayank Kumar <mayank12999@gmail.com>
2026-02-13 13:51:59 +0530

    Enabled CORS for frontend access

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routes.predict import router
 
 app = FastAPI()
 
-app.include_router(router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+app.include_router(router)
 
 @app.get("/")
 def root():
     return {"status": "FluxLane backend running"}
-
 
 @app.head("/")
 def root_head():
